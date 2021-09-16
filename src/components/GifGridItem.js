1e2f385868408f0a85a30bcfb979380190eaaea8
Erick Fariñas <efarinas99@gmail.com>
2021-09-16 11:46:47 -0600

    [test] gif item tests

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { PropTypes } from 'prop-types'
 
 export const GifGridItem = ({id, title, url}) => {
     console.log(id, title, url)
     return (
-        <div className="card animate__fadeIn">
+        <div className="card animate__animated animate__fadeIn">
             <img className="card-img-top" src={url} alt={title} />
             <p>{title}</p>
         </div>
     )
+}
+
+GifGridItem.propTypes ={
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
 }
--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -3,9 +3,31 @@ import React from "react"
 import { GifGridItem } from "../../components/GifGridItem"
 
 describe('Pruebas en <GifGridItem />', () =>{
+    const title = 'title';
+    const url = 'https://localhost/test.jpg';
+    const id = '123';
+    const wrapper = shallow(<GifGridItem title={title} url={url} id={id} />)
+
     test('should show the component successfully', () => {
-        const wrapper = shallow(<GifGridItem/>)
         expect( wrapper ).toMatchSnapshot();
+    })
+
+    test('should show the title paragraph', () => {
+        const paragraph = wrapper.find('p');
+        expect( paragraph.text().trim() ).toBe( title );
+    })
+
+    test('should have the image equal to the url and alt of the props', () => {
+        const img = wrapper.find('img');
+        
+        expect( img.prop('src') ).toBe( url );
+        expect( img.prop('alt') ).toBe( title );
+    })
+
+    test('should have animate__fadeIn', () => {
+        const div = wrapper.find('div');
+        const className = div.prop('className');
+        expect( className.includes('animate__fadeIn') ).toBe( true );
     })
     
 })
